1b6aae27bc89974f209466c41f241f41629dddda
ndduc <ndduc1856@gmail.com>
2024-01-13 10:18:13 -0700

    added git ignore

--- Ebacon/obj/Debug/net8.0/Ebacon.AssemblyInfo.cs
+++ Ebacon/obj/Debug/net8.0/Ebacon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ebacon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d02134894efbd902c7e2f3238302bcf4bec8e8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ebacon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ebacon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
